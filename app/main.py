0c972578c6b2f786e5ff23bbf4cc2f4b630457fa
Alejandro Macias <alex.mac.fon@gmail.com>
2025-03-24 11:44:35 -0600

    feat: added boilerplate for main file

---
+++ app/main.py
@@ -0,0 +1,18 @@
+import sys
+import json
+from jsonschema import validate
+
+def main():
+    if len(sys.argv) != 2:
+        print("Use: python3 main.py <json file>")
+        sys.exit(1)
+    json_file = sys.argv[1]
+    try:
+        with open(json_file, 'r') as f:
+            automaton = json.load(f)
+    except FileNotFoundError:
+        print("File not found")
+    except json.JSONDecodeError:
+        print("File not valid")
+if __name__ == "__main__":
+    main()
