54c5823cf3a508f1a3139a29c69225e6f1585305
Alejandro Macias <alex.mac.fon@gmail.com>
2025-03-24 12:11:40 -0600

    refactor: passed draw as function module

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import sys
 import json
 from jsonschema import validate
+import draw_automaton as drw
 
 def main():
     if len(sys.argv) != 2:
@@ -14,5 +15,6 @@ def main():
         print("File not found")
     except json.JSONDecodeError:
         print("File not valid")
+    drw.draw(automaton)
 if __name__ == "__main__":
     main()
