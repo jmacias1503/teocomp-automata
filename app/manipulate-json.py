fb40d1f52fdc2ffbc64822733e9c7d149e188216
David <dmataguerra@outlook.com>
2025-03-24 11:35:54 -0600

    Added autoata.json, manipulate.py

---
+++ app/manipulate-json.py
@@ -0,0 +1,31 @@
+import json
+from graphviz import Digraph
+
+# Read the JSON file
+with open("../automata.json", encoding="utf-8") as read_file:
+    frie_data = json.load(read_file)
+
+# Save the values of the JSON file
+alphabet = frie_data.get("alphabet", [])
+states = frie_data.get("states", [])
+initial_state = frie_data.get("initial_state", "")
+final_states = frie_data.get("final_states", [])
+transitions = frie_data.get("transitions", {})
+
+# Draw the automata
+dot = Digraph()
+
+for state in states:
+    if state in final_states:
+        dot.node(state, shape='doublecircle')  
+    else:
+        dot.node(state)  
+
+dot.node('_start', shape="none", label="")
+dot.edge('_start', initial_state)
+
+for from_state, transition in transitions.items():
+    for symbol, to_state in transition.items():
+        dot.edge(from_state, to_state, label=symbol)
+
+dot.render('finite_automaton.gv', view=True)
